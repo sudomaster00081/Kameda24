e2f13c84fd2b0ffc14f514e8d27f3fc26335ed1b
sudomaster00081 <ajayprasadd0008@gmail.com>
2024-02-08 13:32:50 +0530

    +Added ChatBot mdified

---
+++ ChatBot modi/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+
+department = ''
+date =''
+doctor =''
+time=''
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    if msg != '':
+        input = msg
+        return get_Chat_response(input)
+
+
+# Updated get_Chat_response function
+def get_Chat_response(text):
+    print(text)
+    # Sample response with chat and options
+    response = {
+        'chat': 'Hey, how can I help you?',
+        'options': ['Option 1', 'Option 2', 'Option 3']
+    }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run()
